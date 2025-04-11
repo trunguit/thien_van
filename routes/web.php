d4b22b0ab88a8467c38345de1a0e5e281031ef6c
Trung <ola1995.01@gmail.com>
2025-04-12 01:47:38 +0700

    add route auto deloy

--- routes/web.php
+++ routes/web.php
@@ -109,3 +109,7 @@ Route::get('tim-kiem', [HomeController::class, 'search'])->name('frontend.search
 Route::get('trang/{alias?}.html', [HomeController::class, 'page'])->name('frontend.page');
 
 Route::post('yeu-cau-bao-gia.html', [FrontendCategoryController::class, 'quotes'])->name('frontend.quotes.store');
+Route::post('/deploy-hook', function () {
+    include base_path('deploy-hook.php');
+    return response()->json(['message' => 'Deployed!']);
+});
