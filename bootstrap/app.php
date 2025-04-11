5cfc95f7a809ee1e9bda6153705df7f6bd43eaab
Trung <ola1995.01@gmail.com>
2025-04-12 03:08:03 +0700

    test

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,8 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->validateCsrfTokens(except: [
-            '/admin/tinymce/upload-image'
+            '/admin/tinymce/upload-image',
+            'deploy-hook'
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
